6d9fd2c34b172c66b18ccb4029db786e03113475
kirillsakun <kirillsakun03@gmail.com>
2023-07-14 12:57:20 +0200

    Content: Add 1st version of web components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
 	modules: ['@nuxtjs/tailwindcss', '@nuxt/content', 'nuxt-icons'],
+	vue: {
+		compilerOptions: {
+			isCustomElement: (tag) => tag.includes('-'),
+		},
+	},
 	css: ['~/assets/css/tailwind.css'],
 	app: {
-		pageTransition: { name: 'page', mode: 'out-in' }
+		pageTransition: { name: 'page', mode: 'out-in' },
 	},
 	tailwindcss: {
 		// add '~tailwind.config` alias
-		exposeConfig: true
+		exposeConfig: true,
 	},
 	content: {
 		highlight: {
 			theme: 'material-ocean',
-		}
-	}
+		},
+	},
 });
